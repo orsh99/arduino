55b9d204173adb4a74f1d307f3417006b6f16034
Or Sharabi <orsh99@gmail.com>
2021-12-30 09:11:46 +0200

    read 2 temperatures

--- temperature_monitor/temperature_monitor.ino
+++ temperature_monitor/temperature_monitor.ino
@@ -1,9 +1,9 @@
 #include <LiquidCrystal.h>
 
 
-int time = 50;
 LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
-int thermistorPin = 0;
+int thermistor1Pin = 0;
+int thermistor2Pin = 1;
 
 void setup() {
     lcd.begin(16, 2);
@@ -11,15 +11,21 @@ void setup() {
 }
 
 void loop() {
-    float a = analogRead(thermistorPin);
-    float resistor = (1023.0 * 10000) / a - 10000;
-    float tempC = (3435.0 / (log(resistor / 10000) + (3435.0 / (273.15 + 25)))) - 273.15;
+    float a = analogRead(thermistor1Pin);
+    float resistor1 = (1023.0 * 10000) / a - 10000;
+    float temp1C = (3435.0 / (log(resistor1 / 10000) + (3435.0 / (273.15 + 25)))) - 273.15;
+
+    float b = analogRead(thermistor2Pin);
+    float resistor2 = (1023.0 * 10000) / b - 10000;
+    float temp2C = (3435.0 / (log(resistor2 / 10000) + (3435.0 / (273.15 + 25)))) - 273.15;
     
     lcd.setCursor(0, 0);
-    lcd.print("   this is me   ");
+    lcd.print(" Temp1: ");
+    lcd.print(temp1C);
+    lcd.print(" C  ");
     lcd.setCursor(0, 1);
-    lcd.print("  Temp: ");
-    lcd.print(tempC);
+    lcd.print(" Temp2: ");
+    lcd.print(temp2C);
     lcd.print(" C  ");
     delay(500);
 }
